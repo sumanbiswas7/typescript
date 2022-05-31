481546c5d65532004933c508abca4118ee43a776
suman <ronaldosuman97@gmail.com>
2022-05-31 20:58:45 +0530

    basicTypes, arrays_tuples

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-const alo = "Hellsso";
+// Basic types
+const user = "monica";
+const age = 18;
+const isMarried = false;
+// Arrays and Tuples
+const friends = ["sammuel", "rick"];
+const sammuelMobile = ["sammuel", 812933004];
+let friendsNumber;
+friendsNumber = [
+    ["sammuel", 812933004],
+    ["rick", 0922132123],
+];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-const alo: string = "Hellsso";
+// Basic types
+const user: string = "monica";
+const age: number = 18;
+const isMarried: boolean = false;
+
+// Arrays and Tuples
+
+const friends: string[] = ["sammuel", "rick"];
+const sammuelMobile: [string, number] = ["sammuel", 812933004];
+let friendsNumber: [string, number][];
+friendsNumber = [
+  ["sammuel", 812933004],
+  ["rick", 0922132123],
+];
